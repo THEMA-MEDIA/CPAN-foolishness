caf07de627bc9eb378964353cd15ca027f117f1e
Theo van Hoesel <Th.J.v.Hoesel@THEMA-MEDIA.nl>
2014-10-07 13:21:46 +0200

    Rewrote the whole module, to include POD and removed all code

---
+++ CPAN-foolishness/lib/THEMA/MEDIA.pm
@@ -0,0 +1,71 @@
+package THEMA::MEDIA;
+
+=head1 NAME
+
+THEMA-MEDIA - Providing solutions with Modern Perl
+
+=head1 SYNOPSIS
+
+  
+  #! /usr/bin/env perl
+  
+  use strict;
+  use warnings;
+  
+  use v5.20.1;
+  use feature "signatures";
+  
+  use THEMA::MEDIA;
+  
+
+=head1 ABSTRACT
+
+Much has changed in twenty years...
+But THEMA-MEDIA still provides solutions based on proven technologies
+
+=head1 DESCRIPTION
+
+... I will come up with a proper description
+
+=head1 EXPORT
+
+none, there is no code yet
+
+=cut
+
+
+                                        
+                    ########            
+                    ########            
+                    ########  ########  
+          ########  ########  ########  
+          ########            ########  
+          ########  ########  ########  
+          ########  ########            
+                    ########  ########  
+          ########  ########  ########  
+          ########            ########  
+          ########  ########  ########  
+          ########  ########            
+                    ########            
+                    ########            (c) 1994  www.THEMA-MEDIA.nl
+                                        
+
+
+
+
+=head1 AUTHOR
+
+Theo van Hoesel, <Th.J.v.Hoesel@THEMA-MEDIA.nl>
+
+=head1 COPYRIGHT AND LICENSE
+
+(c) 2014 by THEMA-MEDIA
+
+This library is free software; you can redistribute it and/or modify
+it under the same licence as Perl itself.
+
+=cut
+
+1;
+
